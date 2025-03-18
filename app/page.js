067b7215a33710c599d5078cc0328bcefe70d3bc
Anto_Abisheik <114017992+Anto-099-New-State@users.noreply.github.com>
2025-03-18 08:12:41 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,19 +4,16 @@ import RecentlyAdded from '@/components/RecentlyAdded';
 import PaperList from '@/components/PaperList';
 import Globalapi from '@/app/Gloabalapi/Globalapi'
 import ConnectButton from '@/components/ConnectButton'
+import Link from 'next/link'
+
 export default async function Home() {
   const uploads = await Globalapi.getRecent();
   const myUploads = await Globalapi.myPapers();
   return (
 
     <div className="flex min-h-screen bg-[#faf8ff]">
-      <Sidebar />
-      <main className="flex-1 p-8">
-           <ConnectButton className='absolute top-4 left-4 z-10' />
-        <div className="max-w-6xl mx-auto space-y-8">
-          <SearchBar />
-          <RecentlyAdded uploads={uploads}/>
-          <PaperList myUploads={myUploads} />
+           <h1> Navigate to  </h1> <p>(unfortunately the deployed link has been depreciated so , i new link has been created,sorry for the  inconvience)</p>
+            <Link href="https://desci-hack-sqdi.vercel.app">DesciHacks</Link>
         </div>
       </main>
     </div>
